cc6421197604924e11897546109e4072bae22497
sakshijain20 <jain.sakshi1099@gmail.com>
2024-05-28 13:45:27 +0530

    init-commit

---
+++ music-streaming-app/src/main/java/coms/music_streaming_app/MusicStreamingAppApplication.java
@@ -0,0 +1,13 @@
+package coms.music_streaming_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicStreamingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicStreamingAppApplication.class, args);
+	}
+
+}
---
+++ music-streaming-app/src/test/java/coms/music_streaming_app/MusicStreamingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package coms.music_streaming_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStreamingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
